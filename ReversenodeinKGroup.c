41f5f14112b58b1047b2d5539319957287d19aaa
Jasstin Roy <164056603+Jasstinroy@users.noreply.github.com>
2026-03-06 22:16:03 +0530

    Update ReversenodeinKGroup.c

--- ReversenodeinKGroup.c
+++ ReversenodeinKGroup.c
@@ -1,1 +1,30 @@
+struct ListNode* reverseKGroup(struct ListNode* head, int k) {
+    struct ListNode *curr = head, *prev = NULL, *next = NULL;
+    int count = 0;
 
+    curr = head;
+    while (curr && count < k) {
+        curr = curr->next;
+        count++;
+    }
+
+    if (count == k) {
+        curr = head;
+        count = 0;
+
+        while (curr && count < k) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+
+        if (next)
+            head->next = reverseKGroup(next, k);
+
+        return prev;
+    }
+
+    return head;
+}
