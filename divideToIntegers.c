ab299e6176cd699e39f4ea921d9658cd6a4a5ae5
Jasstin Roy <164056603+Jasstinroy@users.noreply.github.com>
2026-03-09 23:53:48 +0530

    Create divideTwoIntegers.c

---
+++ divideToIntegers.c
@@ -0,0 +1,31 @@
+#include <limits.h>
+#include <stdlib.h>
+
+int divide(int dividend, int divisor) {
+
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+
+    long a = labs(dividend);
+    long b = labs(divisor);
+    long result = 0;
+
+    while (a >= b) {
+
+        long temp = b;
+        long multiple = 1;
+
+        while (a >= (temp << 1)) {
+            temp <<= 1;
+            multiple <<= 1;
+        }
+
+        a -= temp;
+        result += multiple;
+    }
+
+    if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
+        return result;
+    else
+        return -result;
+}
